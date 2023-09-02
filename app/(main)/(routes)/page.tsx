334adbf81217d7002425170296ae0dd8e616f415
mandloideep <mandloideep22@gmail.com>
2023-09-02 00:50:25 -0500

    Prisma Setup and PlanetScale with Profile creation as well.

--- app/(main)/(routes)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-
-export default function Home() {
-  return (
-    <div className="h-full flex items-center justify-center">
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
-}
